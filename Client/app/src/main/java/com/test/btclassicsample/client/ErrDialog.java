660ff00206b118b71e6abfedac82470ea2ce6464
rg687076 <tokushi2011@gmail.com>
2022-02-11 15:13:58 +0900

    エラー用ダイアログに予び元情報をログ出力するように修正。

--- Client/app/src/main/java/com/test/btclassicsample/client/ErrDialog.java
+++ Client/app/src/main/java/com/test/btclassicsample/client/ErrDialog.java
@@ -2,10 +2,15 @@ package com.test.btclassicsample.client;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.util.Log;
+import java.text.MessageFormat;
 
 public class ErrDialog {
 	/* AlertDialog生成 */
 	public static AlertDialog.Builder create(Context context, String ErrStr) {
+		StackTraceElement throwableStackTraceElement = new Throwable().getStackTrace()[1];
+		String head = MessageFormat.format("{0}::{1}({2})", throwableStackTraceElement.getClassName(), throwableStackTraceElement.getMethodName(), throwableStackTraceElement.getLineNumber());
+		Log.d("aaaaa", MessageFormat.format("{0} {1}",head, ErrStr));
 		return new AlertDialog.Builder(context)
 				.setMessage(ErrStr)
 				.setNegativeButton("終了", (dialog, id) -> {
